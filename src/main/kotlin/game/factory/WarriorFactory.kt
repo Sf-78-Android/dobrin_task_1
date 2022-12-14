b07abab26f0e7afa1f932357688b90bc20073065
dobrin_r <rashkov09@gmail.com>
2022-12-14 03:35:42 +0200

    Edit to WarriorFactory

--- src/main/kotlin/game/factory/WarriorFactory.kt
+++ src/main/kotlin/game/factory/WarriorFactory.kt
@@ -2,17 +2,20 @@ package game.factory
 
 import game.characters.*
 import game.interfaces.BaseWarrior
+import java.util.*
 
 // TODO validation and not case sensitive
 fun  getWarrior(type : String) :  BaseWarrior {
          var warrior : BaseWarrior? = null
-        when(type){
-            "Knight" -> warrior = Knight(Warrior())
-            "Vampire" -> warrior = Vampire(Warrior())
-            "Warrior" -> warrior = Warrior()
-            "Lancer" -> warrior = Lancer(Warrior())
-            "Defender" -> warrior = Defender(Warrior())
+    if (type.isNotEmpty() and type.isNotBlank()) {
+        when (type.lowercase(Locale.getDefault())) {
+            "knight" -> warrior = Knight(Warrior())
+            "vampire" -> warrior = Vampire(Warrior())
+            "warrior" -> warrior = Warrior()
+            "lancer" -> warrior = Lancer(Warrior())
+            "defender" -> warrior = Defender(Warrior())
         }
+    }
 
     return  warrior!!
     }
